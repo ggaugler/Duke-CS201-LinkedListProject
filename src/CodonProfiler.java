e4aee13899022933edae9177af855ef3e34dfeba
Ashley Lanzas <ashley.lanzas@duke.edu>
2019-03-20 21:21:39 -0400

    codonprof done?

--- src/CodonProfiler.java
+++ src/CodonProfiler.java
@@ -13,36 +13,58 @@ public class CodonProfiler {
 	 * strand. 
 	 */
 	public int[] getCodonProfile(IDnaStrand strand, String[] codons) {
+		//System.out.println(strand);
+		//for (int i=0;i<codons.length;i++) {
+			//System.out.println(codons[i]);
+		//}
+		
 		HashMap<String,Integer> map = new HashMap<>();
 		int[] ret = new int[codons.length];
 				
-		for(int k=0; k < codons.length; k++) {
-			Iterator<Character> iter = strand.iterator();
-			while (iter.hasNext()) {
-				char a = iter.next();
-				char b = 'z';           // not part of any real codon
-				char c = 'z';
-				if (iter.hasNext()) {
-					b = iter.next();
+//		for(int k=0; k < codons.length; k++) {
+//			Iterator<Character> iter = strand.iterator();
+//			while (iter.hasNext()) {
+//				char a = iter.next();
+//				char b = 'z';           // not part of any real codon
+//				char c = 'z';
+//				if (iter.hasNext()) {
+//					b = iter.next();
+//				}
+//				if (iter.hasNext()) {
+//					c = iter.next();
+//				}
+//				String cod = ""+a+b+c;
+//				if (cod.equals(codons[k])) {
+//					ret[k] += 1;
+//				}
+//			}
+//		}
+		//return ret;
+		
+		
+			for (int i=0; i<strand.size()-2; i+=3) {
+				String s = strand.toString();
+				String cod = s.substring(i,i+3);
+				map.putIfAbsent(cod,0);
+				map.put(cod,map.get(cod)+1);
 				}
-				if (iter.hasNext()) {
-					c = iter.next();
-				}
-				String cod = ""+a+b+c;
-				if (cod.equals(codons[k])) {
-					ret[k] += 1;
-				}
+			
+		System.out.println(map);
+		
+		for (int i=0; i < codons.length; i++) {
+			if (! map.containsKey(codons[i])) {
+				ret[i] = 0;
+			}
+			else {
+				ret[i] = map.get(codons[i]);
 			}
 		}
-		//return ret;
-		for (int k=0; k < codons.length; k++) {
-			map.putIfAbsent(codons[k],0);
-			map.put(codons[k],map.get(codons[k])+1);
+		
+		for (int i=0;i<ret.length;i++) {
+			System.out.println(ret[i]);
 		}
-		for (int k=0; k < codons.length; k++) {
-			ret[k] = map.get(codons[k]);
-		}
+		
 		return ret;
-	}
 	
+	}
 }
